a3c020f6d2b3a0aee76caac365005754462c492a
Jinzhe Zeng <njzjz@qq.com>
2018-09-19 13:30:42 +0800

    update

--- js/shqk.js
+++ js/shqk.js
@@ -12,11 +12,6 @@ $(document).ready(function() {
 		type: 'inline',
 		preloader: false,
 		focus: '#username',
-		modal: true
-	});
-	$(document).on('click', '.popup-modal-dismiss', function (e) {
-		e.preventDefault();
-		$.magnificPopup.close();
 	});
 	var map = new AMap.Map('map', {
         zoom:13,
