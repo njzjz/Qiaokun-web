3d42fbc31f162434fc8c23c1ca1c7b23250baf31
Restyled.io <commits@restyled.io>
2019-10-15 02:51:09 +0000

    Restyled by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -32,110 +32,117 @@ const SitemapWebpackPlugin = require("sitemap-webpack-plugin").default;
 const RobotstxtPlugin = require("robotstxt-webpack-plugin");
 
 module.exports = {
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        include : [ path.resolve(__dirname, "src") ],
-        loader : "babel-loader",
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader",
 
-        options : {
-          plugins : [ "syntax-dynamic-import" ],
+        options: {
+          plugins: ["syntax-dynamic-import"],
 
-          presets : [ [ "@babel/preset-env", {modules : false} ] ]
+          presets: [["@babel/preset-env", { modules: false }]]
         },
 
-        test : /\.js$/
+        test: /\.js$/
       },
       {
-        test : /\.css$/,
-        use : [ {loader : MiniCssExtractPlugin.loader}, "css-loader" ]
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"]
       }
     ]
   },
 
-  output : {filename : "[name].[chunkhash].js", publicPath : "./"},
+  output: { filename: "[name].[chunkhash].js", publicPath: "./" },
 
-  mode : "production",
+  mode: "production",
 
-  optimization : {
-    splitChunks : {
-      cacheGroups :
-          {vendors : {priority : -10, test : /[\\/]node_modules[\\/]/}},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: { priority: -10, test: /[\\/]node_modules[\\/]/ }
+      },
 
-      chunks : "async",
-      minChunks : 1,
-      minSize : 30000,
-      name : true
+      chunks: "async",
+      minChunks: 1,
+      minSize: 30000,
+      name: true
     }
   },
-  plugins : [
-    new MiniCssExtractPlugin(
-        {filename : "[name].[chunkhash].css", chunkFilename : "[id].css"}),
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+      chunkFilename: "[id].css"
+    }),
     new HtmlWebpackPlugin({
-      filename : "index.html",
-      template : __dirname + "/src/index.html",
-      inject : "true",
-      minify : {
-        collapseWhitespace : true,
-        collapseBooleanAttributes : true,
-        collapseInlineTagWhitespace : true,
-        minifyCSS : true,
-        minifyJS : true,
-        removeScriptTypeAttributes : true,
-        removeStyleLinkTypeAttributes : true
+      filename: "index.html",
+      template: __dirname + "/src/index.html",
+      inject: "true",
+      minify: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        collapseInlineTagWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true
       }
     }),
     new HtmlWebpackPlugin({
-      filename : "404.html",
-      template : __dirname + "/src/404.html",
-      inject : "false",
-      minify : {collapseWhitespace : true, collapseBooleanAttributes : true}
+      filename: "404.html",
+      template: __dirname + "/src/404.html",
+      inject: "false",
+      minify: { collapseWhitespace: true, collapseBooleanAttributes: true }
     }),
     new WebpackCdnPlugin({
-      modules : [
-        {name : "jquery", var : "$", path : "dist/jquery.min.js"}, {
-          name : "bootstrap",
-          path : "dist/js/bootstrap.min.js",
-          style : "dist/css/bootstrap.min.css"
+      modules: [
+        { name: "jquery", var: "$", path: "dist/jquery.min.js" },
+        {
+          name: "bootstrap",
+          path: "dist/js/bootstrap.min.js",
+          style: "dist/css/bootstrap.min.css"
         },
-        {name : "jquery.easing", path : "jquery.easing.min.js"}, {
-          name : "magnific-popup",
-          path : "dist/jquery.magnific-popup.min.js",
-          style : "dist/magnific-popup.css"
+        { name: "jquery.easing", path: "jquery.easing.min.js" },
+        {
+          name: "magnific-popup",
+          path: "dist/jquery.magnific-popup.min.js",
+          style: "dist/magnific-popup.css"
         },
         {
-          name : "startbootstrap-creative",
-          path : "js/creative.min.js",
-          style : "css/creative.min.css"
+          name: "startbootstrap-creative",
+          path: "js/creative.min.js",
+          style: "css/creative.min.css"
         },
-        {name : "translater.js", var : "tran", path : "dist/translater.min.js"},
+        { name: "translater.js", var: "tran", path: "dist/translater.min.js" },
         {
-          name : "font-awesome",
-          style : "css/font-awesome.min.css",
-          cssOnly : true
+          name: "font-awesome",
+          style: "css/font-awesome.min.css",
+          cssOnly: true
         }
       ],
-      publicPath : "/node_modules",
-      prodUrl : "//cdn.jsdelivr.net/npm/:name@:version/:path"
+      publicPath: "/node_modules",
+      prodUrl: "//cdn.jsdelivr.net/npm/:name@:version/:path"
     }),
     new GoogleFontsPlugin({
-      fonts : [
-        {family : "Merriweather Sans", variants : [ "400", "700" ]}, {
-          family : "Merriweather",
-          variants :
-              [ "400", "300", "300italic", "400italic", "700", "700italic" ]
+      fonts: [
+        { family: "Merriweather Sans", variants: ["400", "700"] },
+        {
+          family: "Merriweather",
+          variants: ["400", "300", "300italic", "400italic", "700", "700italic"]
         }
       ],
-      local : false
+      local: false
     }),
-    new CnameWebpackPlugin({domain : "qk.njzjz.win"}),
-    new SitemapWebpackPlugin(
-        "http://www.shqkchem.com", [ "/" ],
-        {lastMod : true, changeFreq : "monthly", priority : "1.0"}),
+    new CnameWebpackPlugin({ domain: "qk.njzjz.win" }),
+    new SitemapWebpackPlugin("http://www.shqkchem.com", ["/"], {
+      lastMod: true,
+      changeFreq: "monthly",
+      priority: "1.0"
+    }),
     new RobotstxtPlugin({
-      policy : [ {userAgent : "*", allow : "/"} ],
-      sitemap : "http://www.shqkchem.com/sitemap.xml",
-      host : "http://www.shqkchem.com"
+      policy: [{ userAgent: "*", allow: "/" }],
+      sitemap: "http://www.shqkchem.com/sitemap.xml",
+      host: "http://www.shqkchem.com"
     })
   ]
 };
